0752dc570b46a3fc1118e5c8f03f163e5c81dc54
PriyanshuKumarSinha <priyanshuk9066@gmail.com>
2021-07-31 13:02:55 +0530

    V1.0

---
+++ main.js
@@ -0,0 +1,49 @@
+width = window.innerWidth;
+height = window.innerHeight;
+window.onresize = changeVariables;
+nav = document.querySelector("nav");
+logo = document.querySelector("#logo");
+
+
+function setLogoMargin(width){
+    logo.style.marginRight  = (width-300)/2 +'px';
+}
+
+function changeVariables(){
+    width = window.innerWidth;
+    height = window.innerHeight;
+    console.log(width,height)
+    setLogoMargin(width)
+    setContainerPadding(width);
+
+}
+
+function setContainerPadding(width){
+    Vpadding = 50;
+    Hpadding = 150;
+    if (width<= 1080 & width>830){
+        Hpadding = 100;
+    }
+    else if (width<= 830  & width>500){
+        Hpadding = 50;
+    }
+    else if (width<=500){
+        Hpadding = 20;
+    }
+    nav.style.padding= Vpadding+"px "+Hpadding+"px "+Vpadding+"px "+Hpadding+"px ";
+
+
+
+}
+
+// 1366 695
+// 1161 680 padding decrease
+// 500 680 mobile starts
+
+
+setLogoMargin(width);
+setContainerPadding(width);
+
+
+
+
