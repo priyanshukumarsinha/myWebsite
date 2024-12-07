341b2b13065f83b26b7051b85e8ee28660ccb5d6
priyanshukumarsinha <priyanshuk9066@gmail.com>
2024-12-08 02:17:43 +0530

    feat: add AboutMePage component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import LandingPage from "./components/LandingPage";
+
+import AboutMePage from "./components/AboutMePage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="relative">
       <LandingPage />
+      <AboutMePage />
     </div>
   );
 }
